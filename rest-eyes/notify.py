78a72825d2133297baeae061c20a309bfddf837c
Daniel Xu <dxu@dxuuu.xyz>
2017-11-28 10:33:39 -0800

    rest-eyes: only notify during work hours

--- rest-eyes/notify.py
+++ rest-eyes/notify.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
+import datetime
+import sys
 import notify2
+
+# Only notify during work hours
+now = datetime.datetime.now()
+hour = now.hour
+if hour < 9 or hour > 19:
+    sys.exit(0)
 
 notify2.init("rest-eyes")
 n = notify2.Notification("Rest your eyes", "Try to look away for 20s")
