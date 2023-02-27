58fc27ff87c7de757c584b2e276ee607e50a5a84
Daniel Xu <dxu@dxuuu.xyz>
2023-02-27 12:35:02 -0700

    linux: Support passing extra args to git-format-patch

--- linux-patchset.sh
+++ linux-patchset.sh
@@ -5,8 +5,8 @@
 
 set -eu
 
-if [[ $# != 1 ]]; then
-    echo "Usage: ${0} REF" 1>&2
+if (( $# < 1 )); then
+    echo "Usage: ${0} REF [GIT_FORMAT_PATCH_ARGS..]" 1>&2
     exit 1
 fi
 
@@ -15,7 +15,7 @@ OUTDIR="${ROOT}/outgoing/$(git rev-parse --abbrev-ref HEAD)"
 
 # Note we generate empty To: and Cc: headers so `linux-format-patch.py`
 # does not have to figure out the insertion point.
-git format-patch -o "$OUTDIR" -s --to "" --cc "" "$1"
+git format-patch -o "$OUTDIR" -s --to "" --cc "" "$@"
 
 # Add To: and Cc: based on `get_maintainer.pl`
 LINUX_ROOT="$ROOT" linux-address-patchset.py "$OUTDIR"
